af21bf8ef483397b5daa07013cdfe25ff942a96e
Legocoder123YT <113647896+legocoder123@users.noreply.github.com>
2022-12-16 20:48:15 -0600

    Add files via upload

---
+++ first.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "first.h"
+
+int main() {
+    int i = 0;
+    while (i <= 100) {
+        Jeffrey();
+	    i++;
+    }
+    getchar();
+    return 0;
+}
---
+++ first.h
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int Jeffrey() {
+    printf("CEO, entrepreneur\nBorn in 1964\nJeffrey, Jeffrey Bezos\nCEO, entrepreneur\n");
+    printf("Born in 1964\nJeffrey, Jeffrey Bezos\nC'mon Jeffrey, you can do it\n");
+    printf("Pave the way, put your back into it\nTell us why, show us how\nLook at where you came from, look at you now\n");
+    printf("Zuckerberg and Gates and Buffett\nAmateurs can fucking suck it\n");
+    printf("Fuck their wives, drink their blood\nC'mon, Jeff, get 'em!\nAh!\n");
+}
